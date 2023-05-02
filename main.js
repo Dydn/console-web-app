63d6f02b1dc31688a8fb8cef06d0fbe25372af1d
Dydn <126629784+Dydn@users.noreply.github.com>
2023-05-02 16:49:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var a=100
+var b=20
+var fname="camdon"
+var lname="lomeli"
+var fullname=fname.concat(lname)
+console.log("welcome to console screen")
+console.log(fullname)
+var add=a+b
+console.log(add)
+var mult=a*b
+console.log(mult)
+var sub=a-b
+console.log(sub)
+var divide=a/b
+console.log(divide)
